998acee4e774efe9e6c67a89345e81ffe53aa065
Augusto Marinaro <marinaro.augusto@gmail.com>
2020-02-29 17:39:58 -0300

    Updates

---
+++ precache-manifest.db476a1ab1eb432281bfa4c4cc3c9cc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51c8e4663d493d07d0f8314001565c21",
+    "url": "/voice-image/index.html"
+  },
+  {
+    "revision": "969304678913670f10be",
+    "url": "/voice-image/static/css/main.32d61960.chunk.css"
+  },
+  {
+    "revision": "bb1bb9304c8993ff483d",
+    "url": "/voice-image/static/js/2.111ba7cc.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/voice-image/static/js/2.111ba7cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "969304678913670f10be",
+    "url": "/voice-image/static/js/main.619b9ffb.chunk.js"
+  },
+  {
+    "revision": "6752d3d1ca22825542f5",
+    "url": "/voice-image/static/js/runtime-main.9d9e35bb.js"
+  }
+]);
